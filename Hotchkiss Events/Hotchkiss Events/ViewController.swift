3073572fe5fb356dff053ca8327f606fb4ee0a39
Alex Tam <atam@Alexs-MacBook-Pro.local>
2018-12-24 11:20:35 +0800

    alex

---
+++ Hotchkiss Events/Hotchkiss Events/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hotchkiss Events
+//
+//  Created by Alex Tam on 12/24/18.
+//  Copyright © 2018 Alex Tam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Hotchkiss Events/Hotchkiss EventsTests/Hotchkiss_EventsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Hotchkiss_EventsTests.swift
+//  Hotchkiss EventsTests
+//
+//  Created by Alex Tam on 12/24/18.
+//  Copyright © 2018 Alex Tam. All rights reserved.
+//
+
+import XCTest
+@testable import Hotchkiss_Events
+
+class Hotchkiss_EventsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
