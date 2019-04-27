9502e2988c17388da655ac56b0ee5b30edca8b70
Alex Tam <atam@alexs-macbook-pro.local>
2019-04-26 21:54:36 -0400

    titlescreenpage

---
+++ Hotchkiss Events/Hotchkiss Events/titlePageViewController.swift
@@ -0,0 +1,47 @@
+//
+//  titlePageViewController.swift
+//  Hotchkiss Events
+//
+//  Created by Alex Tam on 4/19/19.
+//  Copyright © 2019 Alex Tam. All rights reserved.
+//
+
+import UIKit
+
+class titlePageViewController: UIViewController {
+
+    @IBOutlet weak var titleObject: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        titleObject.center.y -= 300
+        //titleObject.alpha = 0
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 2.0, animations: {
+            self.titleObject.center.y += 300
+             //self.titleObject.alpha = 1
+        }, completion: { (_) in
+            DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) {
+                self.performSegue(withIdentifier: "titlePageSegue", sender: nil)
+                
+            }
+        })
+
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
