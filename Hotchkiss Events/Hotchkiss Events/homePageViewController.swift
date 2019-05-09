7f7f0c04c6f1d79ddec817f0ffe5022c63d12790
Alex Tam <atam@alexs-macbook-pro.local>
2019-05-08 23:25:34 -0400

    latest work

--- Hotchkiss Events/Hotchkiss Events/homePageViewController.swift
+++ Hotchkiss Events/Hotchkiss Events/homePageViewController.swift
@@ -10,21 +10,27 @@ import UIKit
 class homePageViewController:UIViewController
 {
     @IBOutlet weak var athleticsButton: UIButton?
-    @IBOutlet weak var othersButton: UIButton?
-    @IBOutlet weak var clubEventsButton: UIButton?
     @IBOutlet weak var formalSchoolEventsButton: UIButton?
+    override func viewWillLayoutSubviews() {
+        let bounds = view.bounds
+        let spacing = CGFloat(8.0)
+        var schoolTab = CGRect.zero
+        schoolTab.size.width = bounds.size.width - (2.0 * spacing)
+    }
+    
+    
+    
+    
     
     override func viewDidAppear(_ animated: Bool)
     {
         athleticsButton?.backgroundColor = UIColor.init(displayP3Red: 238.0/255, green: 245.0/255, blue: 254.0/255, alpha: 1.0)
     formalSchoolEventsButton?.backgroundColor = UIColor.init(displayP3Red: 238.0/255, green: 245.0/255, blue: 254.0/255, alpha: 1.0)
-        clubEventsButton?.backgroundColor = UIColor.init(displayP3Red: 238.0/255, green: 245.0/255, blue: 254.0/255, alpha: 1.0)
-        othersButton?.backgroundColor = UIColor.init(displayP3Red: 238.0/255, green: 245.0/255, blue: 254.0/255, alpha: 1.0)
+       
         
         athleticsButton?.addTarget(self, action: #selector(athleticsButtonTap), for: .touchUpInside)
     formalSchoolEventsButton?.addTarget(self, action: #selector(formalSchoolEventsButtonTap), for: .touchUpInside)
-        clubEventsButton?.addTarget(self, action: #selector(clubEventsButtonTap), for: .touchUpInside)
-        othersButton?.addTarget(self, action: #selector(othersButtonTap), for: .touchUpInside)
+        
         
     }
     @objc func athleticsButtonTap()
